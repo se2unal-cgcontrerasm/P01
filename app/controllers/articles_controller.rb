2fc535cf5487f84821066c486678ab1de79c371b
Cristhian Contreras <cgcontrerasm@unal.edu.co>
2018-08-31 02:41:53 +0000

    Tercer Commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,11 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
- 
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -20,6 +24,15 @@ class ArticlesController < ApplicationController
     end
   end
  
+  def update
+   @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
